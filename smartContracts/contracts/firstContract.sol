38f237350e45b749a819681affb83289ff9628e8
voidProgramms <isip_r.o.biktimirov@mpt.ru>
2024-03-13 22:47:47 +0300

    MyEstate contract is compleated

---
+++ smartContracts/contracts/firstContract.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.4.16 <0.9.0;
+
+contract MyShop {
+    mapping(address => uint) balances;
+
+    event Deposit(address indexed _from, uint _amount);
+
+
+    function deposit() external payable  {
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function getBalance() external view returns(uint){
+        return balances[msg.sender];
+    }
+
+    function withdraw(uint _amount) external payable {
+        require(_amount <= balances[msg.sender] || _amount > 0, "Not enough maoney to withdraw(");
+        payable(msg.sender).transfer(_amount);
+        balances[msg.sender] -= _amount;
+    }
+}
+
